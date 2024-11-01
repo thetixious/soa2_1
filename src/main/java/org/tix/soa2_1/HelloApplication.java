cc8c4a262265049a3387ffa994645df1440d221b
Arkadiy <ri.arkadiy@gmail.com>
2024-11-01 16:32:49 +0300

    init commit

---
+++ src/main/java/org/tix/soa2_1/HelloApplication.java
@@ -0,0 +1,9 @@
+package org.tix.soa2_1;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
